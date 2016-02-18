4eebb6f78e7b1f228e7f8a25ddb3481c45e84729
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2016-02-18 18:57:21 -0500

    Update JoystickDrive to default to 75% speed.
    Also added turbo button (driver right bumper).
    Note: currently, spike is also mapped to this button. This will be fixed
    in a future commit.

--- src/org/usfirst/frc/team1261/robot/commands/JoystickDrive.java
+++ src/org/usfirst/frc/team1261/robot/commands/JoystickDrive.java
@@ -11,12 +11,22 @@ public class JoystickDrive extends Command {
 	public static final Joystick JOYSTICK = Robot.oi.getDriverJoystick();
 	public static final int THROTTLE_AXIS = OI.AXIS_LEFT_STICK_Y;
 	public static final int ROTATE_AXIS = OI.AXIS_RIGHT_STICK_X;
+	public static final int TURBO_BUTTON = OI.BUTTON_RIGHT_BUMPER;
 	public static final boolean SQUARED_INPUTS = true;
+
+	public static final double SCALING_FACTOR = 0.75;
+
+	double correctedScalingFactor;
 
 	public JoystickDrive() {
 		// Use requires() here to declare subsystem dependencies
 		// eg. requires(chassis);
 		requires(Robot.driveTrain);
+		if (SQUARED_INPUTS) {
+			correctedScalingFactor = Math.sqrt(SCALING_FACTOR);
+		} else {
+			correctedScalingFactor = SCALING_FACTOR;
+		}
 	}
 
 	// Called just before this Command runs the first time
@@ -26,7 +36,13 @@ public class JoystickDrive extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		Robot.driveTrain.getRobotDrive().arcadeDrive(JOYSTICK, THROTTLE_AXIS, JOYSTICK, ROTATE_AXIS, SQUARED_INPUTS);
+		double throttleValue = JOYSTICK.getRawAxis(THROTTLE_AXIS);
+		double rotateValue = JOYSTICK.getRawAxis(ROTATE_AXIS);
+		if (!JOYSTICK.getRawButton(TURBO_BUTTON)) {
+			throttleValue *= correctedScalingFactor;
+			rotateValue *= correctedScalingFactor;
+		}
+		Robot.driveTrain.getRobotDrive().arcadeDrive(throttleValue, rotateValue, SQUARED_INPUTS);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
