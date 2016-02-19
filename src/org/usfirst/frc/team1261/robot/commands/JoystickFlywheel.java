72691ec16394a4952bcb084b159fb834c9c25541
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2016-02-18 20:14:15 -0500

    Make only manip. right trigger control flywheel
    Previously, manipulator left trigger would also control the flywheel in
    the opposite direction.

--- src/org/usfirst/frc/team1261/robot/commands/JoystickFlywheel.java
+++ src/org/usfirst/frc/team1261/robot/commands/JoystickFlywheel.java
@@ -12,11 +12,9 @@ import edu.wpi.first.wpilibj.command.Command;
 public class JoystickFlywheel extends Command {
 
 	public static final Joystick JOYSTICK = Robot.oi.getManipulatorJoystick();
-	public static final int OUT_JOYSTICK_AXIS = OI.AXIS_RIGHT_TRIGGER;
-	public static final int IN_JOYSTICK_AXIS = OI.AXIS_LEFT_TRIGGER;
+	public static final int JOYSTICK_AXIS = OI.AXIS_RIGHT_TRIGGER;
 
-	public static final double OUT_POWER_SCALING_FACTOR = 1.0;
-	public static final double IN_POWER_SCALING_FACTOR = 0.35;
+	public static final double POWER_SCALING_FACTOR = 1.0;
 
 	public JoystickFlywheel() {
 		// Use requires() here to declare subsystem dependencies
@@ -31,8 +29,7 @@ public class JoystickFlywheel extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		Robot.flywheel.setFlywheelPower((JOYSTICK.getRawAxis(OUT_JOYSTICK_AXIS) * OUT_POWER_SCALING_FACTOR)
-				- (JOYSTICK.getRawAxis(IN_JOYSTICK_AXIS) * IN_POWER_SCALING_FACTOR));
+		Robot.flywheel.setFlywheelPower(JOYSTICK.getRawAxis(JOYSTICK_AXIS) * POWER_SCALING_FACTOR);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
