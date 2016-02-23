a52a6993d0f13e60a9e43f86b9ea2bede8d81acd
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2016-02-23 18:06:42 -0500

    Add tracking commands for vision tracking.
    As opposed to AutoElevate/Align, these don't stop once they've hit their
    target.

---
+++ src/org/usfirst/frc/team1261/robot/commands/TrackAlign.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1261.robot.commands;
+
+/**
+ *
+ */
+public class TrackAlign extends AutoAlign {
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+}
---
+++ src/org/usfirst/frc/team1261/robot/commands/TrackElevate.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1261.robot.commands;
+
+/**
+ *
+ */
+public class TrackElevate extends AutoElevate {
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+}
