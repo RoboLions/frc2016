88f5c9b6b352ff0d3e4f3294102fdf5748c2eed4
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2016-02-22 20:04:29 -0500

    Add VisionTrackingBasedDriveTrainPIDController.
    This allows drivetrain code to use vision tracking for PID.

---
+++ src/org/usfirst/frc/team1261/robot/subsystems/VisionTrackingBasedDriveTrainPIDController.java
@@ -0,0 +1,55 @@
+package org.usfirst.frc.team1261.robot.subsystems;
+
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+
+/**
+ * A vision-tracking-based {@link DriveTrain} {@link PIDController}.
+ */
+class VisionTrackingBasedDriveTrainPIDController extends PIDController {
+
+	// TODO: figure out these values
+	public static final double kP = 0.001;
+	public static final double kI = 0.0;
+	public static final double kD = 0.0;
+	public static final double DEFAULT_TOLERANCE = RaspberryPiCommunicationAdapter.X_AXIS_TOLERANCE;
+
+	/**
+	 * Error value used for PID when no target can be found.
+	 */
+	public static final double DEFAULT_ERROR = 0.0;
+
+	public VisionTrackingBasedDriveTrainPIDController(DriveTrain driveTrain) {
+		super(kP, kI, kD, new DisplacementPIDSource() {
+			@Override
+			public double pidGet() {
+				try {
+					return RaspberryPiCommunicationAdapter.getTargetXOffset();
+				} catch (RaspberryPiCommunicationAdapter.NoContoursFoundException e) {
+					return DEFAULT_ERROR;
+				}
+			}
+		}, new PIDOutput() {
+			@Override
+			public void pidWrite(double output) {
+				driveTrain.turn(output);
+			}
+		});
+		setAbsoluteTolerance(DEFAULT_TOLERANCE);
+	}
+
+	/**
+	 * Return {@code true} if the error is within the tolerance determined by
+	 * {@link VisionTrackingBasedDriveTrainPIDController#DEFAULT_TOLERANCE}.<br>
+	 * <em>This method overrides {@link PIDSubsystem}'s
+	 * {@link PIDSubsystem#onTarget onTarget} method as a workaround for
+	 * <a href="https://usfirst.collab.net/sf/tracker/do/viewArtifact/projects.wpilib/tracker.4_defects/artf4812">
+	 * a bug in WPILib's implementation</a>.</em>
+	 * 
+	 * @return {@code true} if the error is less than the tolerance.
+	 */
+	public boolean onTarget() {
+		return (Math.abs(getError()) < DEFAULT_TOLERANCE);
+	}
+}
