30b336c4acd3e10c254d9ec2b49359843538d912
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2016-03-15 20:05:11 -0400

    Add constants for shooter positions in SimpleAuto.
    Added SHOOTER_ARM_INITIAL_POSITION and SHOOTER_ARM_FINAL_POSITION
    constants in SimpleAutonomousProgram.

--- src/org/usfirst/frc/team1261/robot/commands/SimpleAutonomousProgram.java
+++ src/org/usfirst/frc/team1261/robot/commands/SimpleAutonomousProgram.java
@@ -9,9 +9,10 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class SimpleAutonomousProgram extends CommandGroup {
 
-	public static final double SHOOTER_ARM_POSITION = ShooterArm.SETPOINT_HORIZONTAL_POSITION;
+	public static final double SHOOTER_ARM_INITIAL_POSITION = ShooterArm.SETPOINT_HORIZONTAL_POSITION;
+	public static final double SHOOTER_ARM_FINAL_POSITION = ShooterArm.SETPOINT_SHOOTING_POSITION;
 	public static final double INITIAL_DRIVE_FORWARD_DURATION = 2.9;
-	public static final double DRIVE_FORWARD_UNTIL_RANGE_FINDER_DISTANCE_TIMEOUT = .5;
+	public static final double DRIVE_FORWARD_UNTIL_RANGE_FINDER_DISTANCE_TIMEOUT = 0.5;
 	public static final double RANGE_FINDER_DISTANCE = 1.4;
 
 	public SimpleAutonomousProgram() {
@@ -31,12 +32,12 @@ public class SimpleAutonomousProgram extends CommandGroup {
 		// e.g. if Command1 requires chassis, and Command2 requires arm,
 		// a CommandGroup containing them would require both the chassis and the
 		// arm.
-		addSequential(new GoToShooterArmPosition(SHOOTER_ARM_POSITION));
+		addSequential(new GoToShooterArmPosition(SHOOTER_ARM_INITIAL_POSITION));
 		addSequential(new DriveForward(INITIAL_DRIVE_FORWARD_DURATION));
 		addSequential(new DriveForwardUntilRangeFinderDistance(RANGE_FINDER_DISTANCE),
 				DRIVE_FORWARD_UNTIL_RANGE_FINDER_DISTANCE_TIMEOUT);
 		addSequential(new IntakeArmToLowerLimitSwitch());
-		addSequential(new GoToShooterArmPosition(ShooterArm.SETPOINT_SHOOTING_POSITION));
+		addSequential(new GoToShooterArmPosition(SHOOTER_ARM_FINAL_POSITION));
 		//addSequential(new AutoElevateAlign());
 	}
 }
