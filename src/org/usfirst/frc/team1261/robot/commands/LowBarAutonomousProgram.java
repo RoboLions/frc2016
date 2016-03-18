d8159c160de6f6fb7441e47113af4c66059273e0
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2016-03-18 15:13:32 -0400

    Added timeout to lower intake arm in low bar auto.
    The current timeout is set to 1 second.

--- src/org/usfirst/frc/team1261/robot/commands/LowBarAutonomousProgram.java
+++ src/org/usfirst/frc/team1261/robot/commands/LowBarAutonomousProgram.java
@@ -12,6 +12,7 @@ public class LowBarAutonomousProgram extends CommandGroup {
 	public static final double DRIVING_POWER = 0.6;
 	public static final double SHOOTER_ARM_INITIAL_POSITION = ShooterArm.SETPOINT_INTAKE_POSITION;
 	public static final double SHOOTER_ARM_FINAL_POSITION = ShooterArm.SETPOINT_SHOOTING_POSITION;
+	public static final double LOWER_INTAKE_ARM_TIMEOUT = 1.0;
 	public static final double DRIVE_FORWARD_UNTIL_LEVEL_TIMEOUT = 3.1;
 	public static final double DRIVE_FORWARD_UNTIL_RANGE_FINDER_DISTANCE_TIMEOUT = 0.5;
 	public static final double RANGE_FINDER_DISTANCE = 1.4;
@@ -33,7 +34,7 @@ public class LowBarAutonomousProgram extends CommandGroup {
 		// e.g. if Command1 requires chassis, and Command2 requires arm,
 		// a CommandGroup containing them would require both the chassis and the
 		// arm.
-		addSequential(new IntakeArmToLowerLimitSwitch());
+		addSequential(new IntakeArmToLowerLimitSwitch(), LOWER_INTAKE_ARM_TIMEOUT);
 		addSequential(new GoToShooterArmPosition(SHOOTER_ARM_INITIAL_POSITION));
 		addSequential(new DriveForwardUntilLevel(DRIVING_POWER), DRIVE_FORWARD_UNTIL_LEVEL_TIMEOUT);
 		addSequential(new DriveForwardUntilRangeFinderDistance(RANGE_FINDER_DISTANCE, DRIVING_POWER),
