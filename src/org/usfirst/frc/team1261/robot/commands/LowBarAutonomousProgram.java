b2344daace042c2c5d536abf5936762a4665b6eb
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2016-03-26 10:31:06 -0400

    Remove final steps of LowBarAutonomousProgram.
    The LowBarAutonomousProgram no longer contains IntakeArmToLowerLimitSwitch
    (which is redundant) and GoToShooterArmPosition (which is unnecessary) at
    the end.

--- src/org/usfirst/frc/team1261/robot/commands/LowBarAutonomousProgram.java
+++ src/org/usfirst/frc/team1261/robot/commands/LowBarAutonomousProgram.java
@@ -11,7 +11,6 @@ public class LowBarAutonomousProgram extends CommandGroup {
 
 	public static final double DRIVING_POWER = 0.6;
 	public static final double SHOOTER_ARM_INITIAL_POSITION = ShooterArm.SETPOINT_INTAKE_POSITION;
-	public static final double SHOOTER_ARM_FINAL_POSITION = ShooterArm.SETPOINT_SHOOTING_POSITION;
 	public static final double LOWER_INTAKE_ARM_TIMEOUT = 1.0;
 	public static final double DRIVE_FORWARD_UNTIL_LEVEL_TIMEOUT = 3.1;
 	public static final double DRIVE_FORWARD_UNTIL_RANGE_FINDER_DISTANCE_TIMEOUT = 0.5;
@@ -39,8 +38,6 @@ public class LowBarAutonomousProgram extends CommandGroup {
 		addSequential(new DriveForwardUntilLevel(DRIVING_POWER), DRIVE_FORWARD_UNTIL_LEVEL_TIMEOUT);
 		addSequential(new DriveForwardUntilRangeFinderDistance(RANGE_FINDER_DISTANCE, DRIVING_POWER),
 				DRIVE_FORWARD_UNTIL_RANGE_FINDER_DISTANCE_TIMEOUT);
-		addSequential(new IntakeArmToLowerLimitSwitch());
-		addSequential(new GoToShooterArmPosition(SHOOTER_ARM_FINAL_POSITION));
 		// addSequential(new AutoElevateAlign());
 	}
 }
